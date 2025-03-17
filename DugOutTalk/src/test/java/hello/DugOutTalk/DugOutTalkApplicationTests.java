31ae1e882691fb1cc47bb6800c2761a81ec63075
GodMinseok <mseok830@naver.com>
2025-03-17 23:47:41 +0900

    DugOutTalk_Project_Start

---
+++ DugOutTalk/src/test/java/hello/DugOutTalk/DugOutTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.DugOutTalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DugOutTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
