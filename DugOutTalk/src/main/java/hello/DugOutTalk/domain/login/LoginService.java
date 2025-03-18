795009c097d98f3fe591ce29d0da935cd169ea6f
GodMinseok <mseok830@naver.com>
2025-03-18 21:15:31 +0900

    LoginService 생성

---
+++ DugOutTalk/src/main/java/hello/DugOutTalk/domain/login/LoginService.java
@@ -0,0 +1,25 @@
+package hello.DugOutTalk.domain.login;
+
+
+import hello.DugOutTalk.domain.member.Member;
+import hello.DugOutTalk.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * @return null 로그인 실패
+     */
+    public Member login(String loginId, String password) {
+
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+
+    }
+}
