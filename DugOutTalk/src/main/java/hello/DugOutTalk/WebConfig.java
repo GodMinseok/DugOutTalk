00ad8852e765eb6bd33168897aaada2e0c0f24f0
GodMinseok <mseok830@naver.com>
2025-03-20 20:29:58 +0900

    인터셉터와 핸들러 메서드의 Argument Resolver를 추가 및 추가 조건 생성

---
+++ DugOutTalk/src/main/java/hello/DugOutTalk/WebConfig.java
@@ -0,0 +1,36 @@
+package hello.DugOutTalk;
+
+
+import hello.DugOutTalk.web.argumentresolver.LoginMemberArgumentResolver;
+import hello.DugOutTalk.web.interceptor.LogInterceptor;
+import hello.DugOutTalk.web.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error");
+    }
+
+}
