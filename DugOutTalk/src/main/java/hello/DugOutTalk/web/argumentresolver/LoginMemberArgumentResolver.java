803f02c12c3233aed064673387e43c6a82a6e458
GodMinseok <mseok830@naver.com>
2025-03-20 20:06:06 +0900

    Spring MVC에서 컨트롤러의 메서드 파라미터 값을 자동으로 주입하는 역할 생성

---
+++ DugOutTalk/src/main/java/hello/DugOutTalk/web/argumentresolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,41 @@
+package hello.DugOutTalk.web.argumentresolver;
+
+
+import hello.DugOutTalk.domain.member.Member;
+import hello.DugOutTalk.web.SessionConst;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+
+
+@Slf4j
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        log.info("supportsParameter 실행");
+
+        boolean hasLoginAnnotation = methodParameter.hasParameterAnnotation(Login.class);
+        boolean hasMemberType = Member.class.isAssignableFrom(methodParameter.getParameterType());
+
+        return hasLoginAnnotation && hasMemberType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest webRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
+
+        log.info("resolveArgument 실행");
+
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return null;
+        }
+        return session.getAttribute(SessionConst.LOGIN_MEMBER);
+    }
+}
