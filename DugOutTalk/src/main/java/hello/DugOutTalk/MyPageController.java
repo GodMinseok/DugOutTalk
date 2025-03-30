908d8d6226ecb423fd37cb8baf4af57c99d8e605
GodMinseok <mseok830@naver.com>
2025-03-30 14:54:37 +0900

    마이 페이지 매핑을 위한 컨트롤러 생성

---
+++ DugOutTalk/src/main/java/hello/DugOutTalk/MyPageController.java
@@ -0,0 +1,24 @@
+package hello.DugOutTalk;
+
+import hello.DugOutTalk.domain.login.Login;
+import hello.DugOutTalk.domain.member.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MyPageController {
+    @GetMapping("/myPage")
+    public String myPage(@Login Member loginMember, Model model) {
+        // 로그인된 사용자가 없다면 로그인 페이지로 리디렉션
+        if (loginMember == null) {
+            return "redirect:/login";
+        }
+
+        // 로그인된 사용자의 정보를 마이페이지에 전달
+        model.addAttribute("member", loginMember);
+        return "myPage";
+    }
+}
