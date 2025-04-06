e37bef6798842d30d84c0f5da8e10f3f09ddd3df
GodMinseok <mseok830@naver.com>
2025-04-06 14:43:32 +0900

    DB에 좋아요 저장할 수 있도록 PostLikeRepository 생성

---
+++ DugOutTalk/src/main/java/hello/DugOutTalk/web/like/PostLikeRepository.java
@@ -0,0 +1,12 @@
+package hello.DugOutTalk.web.like;
+
+import hello.DugOutTalk.domain.member.Member;
+import hello.DugOutTalk.web.post.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+    Optional<PostLike> findByUserAndPost(Member user, Post post);
+}
+
